76a2afb5dbda18c8adf49e2624095d328ce15642
lizzycronin <croel-26@rhodes.edu>
2026-02-17 16:18:26 -0600

    implement login

---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.services';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: { email: string; password: string }) {
+    const user = await this.authService.validateUser(body.email, body.password);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    return { ok: true, userId: user.id };
+  }
+}
